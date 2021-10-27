962778ba22da05eb92588b8d1f641fbdf8afe861
Sujikrishnan <93249884+Sujikrishnan@users.noreply.github.com>
2021-10-27 15:09:30 +0530

    Add files via upload

---
+++ SUJI.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+//variables here
+int suji=0;
+//**************************main*********************
+void main()
+{
+clrscr();
+	    printf("hello world");
+	    printf("%d,suji");
+getch();
+}
+//***************************end**********************
